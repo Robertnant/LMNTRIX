db19440b827abd15868b7c245c7b018b1dc982dc
RobertNant <robertnantchouangn@gmail.com>
2020-03-10 00:13:16 +0100

    Set up camera movement v1.0 and Camera follow

---
+++ Brain-Byte/BrainByte/Assets/Scripts/Photon/GameControllers/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    public float smoothSpeed = 0.125f;
+
+    private void LateUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
