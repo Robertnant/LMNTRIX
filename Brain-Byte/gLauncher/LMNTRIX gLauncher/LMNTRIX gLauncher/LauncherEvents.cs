30d01e292a3668a9ec8a8b085cc77131a0bbe294
RobertNant <robertnantchouangn@gmail.com>
2020-03-09 03:52:08 +0100

    Modified UI

---
+++ Brain-Byte/gLauncher/LMNTRIX gLauncher/LMNTRIX gLauncher/LauncherEvents.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace LMNTRIX_gLauncher
+{
+    class LauncherEvents
+    {
+        public static void LaunchGame()
+        {
+            try
+            {
+                Process.Start("MyFirstGame.exe");
+                //string should later be changed to brainbyte.exe
+                Environment.Exit(0);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Executable failed to open");
+                throw e;
+            }
+        }
+
+        public static void ViewWebsite(string url)
+        {
+            Process.Start(url);
+        }
+    }
+}
