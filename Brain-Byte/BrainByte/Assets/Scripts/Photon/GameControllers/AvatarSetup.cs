9bdf3e61ce538dbd4b4caaf270c43d1f4b714c0a
RobertNant <robertnantchouangn@gmail.com>
2020-03-11 18:10:59 +0100

    Partially set up Character selection. Partially fixed character position bug.

---
+++ Brain-Byte/BrainByte/Assets/Scripts/Photon/GameControllers/AvatarSetup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class AvatarSetup : MonoBehaviour
+{
+    private PhotonView PV;
+    public GameObject myCharacter;
+    public int characterValue;
+
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        
+        if(PV.IsMine)
+        {
+            PV.RPC("RPC_AddCharacter", RpcTarget.AllBuffered, PlayerInfo.PI.selectedCharacter);
+        }
+    }
+
+    [PunRPC]
+    void RPC_AddCharacter(int characterIndex)
+    {
+        characterValue = characterIndex;
+        myCharacter = Instantiate(PlayerInfo.PI.allCharacters[characterIndex], transform.position, transform.rotation,
+            transform);
+    }
+}
