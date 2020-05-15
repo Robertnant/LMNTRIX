ced5312c5fa31677601bcb4482541753c37473f8
Robertnant <robertnantchouangn@gmail.com>
2020-05-15 03:42:26 +0200

    Serialize player health

--- Brain-Byte/BrainByte/Assets/Scripts/Photon/GameControllers/HeadsUpDisplay.cs
+++ Brain-Byte/BrainByte/Assets/Scripts/Photon/GameControllers/HeadsUpDisplay.cs
@@ -1,14 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class HeadsUpDisplay : MonoBehaviour
+public class HeadsUpDisplay : MonoBehaviourPunCallbacks, IPunObservable
 {
     public int playerHealth;
     public int maxHealth;
     public int playerDamage;
     public HealthBar healthBar;
     public bool valsSet = false;
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(playerHealth);
+            Debug.Log("I am the local client: " + GetComponent<PhotonView>().ViewID);
+        }
+        else
+        {
+            playerHealth = (int)stream.ReceiveNext();
+            Debug.Log("I am the remote client: " + GetComponent<PhotonView>().ViewID);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
